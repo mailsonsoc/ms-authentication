339caeccca612ac468e8555da40d6fdfed24e2c4
mailsonsoc <albuquerque.soc@gmail.com>
2022-01-18 13:47:45 -0300

    finalização de base do microsserviço

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
 import userRoute from './routes/user.route';
 
 const app = express();
 //configurações da aplicação
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //configurações de rotas
 app.use(userRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
-    res.status(200).send({foo: 'Sucesso!'});
-});
+app.use(statusRoute);
 
 //inicialização do servidor
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 })
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -21,8 +21,18 @@ usersRoute.get('/users/:uuid',(req: Request<{uuid: string}>, res: Response, next
 
 usersRoute.post('/users',(req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-    console.log(req.body);
     res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send(modifiedUser);
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.OK);
 });
 
 export default usersRoute;
